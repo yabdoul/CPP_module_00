54164d93a4feff8798439ec5df23209b10aa94dd
Youssef Abdoul <yabdoul@c3r5p2.1337.ma>
2025-03-20 16:21:38 +0000

    first_push

--- ex01/contact.cpp
+++ ex01/contact.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yabdoul <yabdoul@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/18 00:28:19 by yabdoul           #+#    #+#             */
+/*   Updated: 2025/03/18 00:28:20 by yabdoul          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "contact.hpp"  
  #include <iostream>  
 Contact::Contact(std::string f_name ,  std::string l_name ,  std::string  n_name  , std::string  secret ): first_name(f_name) ,last_name(l_name),nick_name(n_name),darkest_secret(secret) { 
--- ex01/contact.hpp
+++ ex01/contact.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yabdoul <yabdoul@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/18 00:28:21 by yabdoul           #+#    #+#             */
+/*   Updated: 2025/03/18 02:32:44 by yabdoul          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string>   
 #ifndef CONTACT_H
 #define CONTACT_H
@@ -13,9 +25,9 @@ class Contact
         }  ;      
         ~Contact()  ;   
         Contact(std::string f_name ,  std::string l_name ,  std::string  n_name  , std::string  secret ) ;    
-        std::string  get_first_name();     
-        std::string  get_last_name();   
-        std::string  get_nickname();    
-        std::string  get_darkest_secret();      
+        std::string  get_first_name() const ;     
+        std::string  get_last_name()  const ;   
+        std::string  get_nickname()   const ;    
+        std::string  get_darkest_secret() const ;      
    } ;     
 #endif   
